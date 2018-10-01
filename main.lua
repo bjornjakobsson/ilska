16eaeae4be342acccb41b1228abbcca15b407d83
Björn Jakobsson <bjornjak10@gmail.com>
2018-10-01 09:49:16 +0200

    var

--- main.lua
+++ main.lua
@@ -1,8 +1,48 @@
+SLASH_ILSKA1 = "/ilska"
+
+
+local visibleToggle = 1
+
+
+SlashCmdList["ILSKA"] = function(msg)
+	if visibleToggle == 1 then
+		Ragebar:Hide();
+		visibleToggle = 2
+	else 
+		Ragebar:Show();
+		visibleToggle = 1
+	end
+end 
+function IlskaStartResize(self)
+	self:SetScript("OnUpdate", IlskaResize);
+end
+function IlskaStopResize(self)
+	self:SetScript("OnUpdate", nil);
+end
+function IlskaResize(self)
+
+	local parent = self:GetParent();
+	local cursorX, cursorY = GetCursorPosition(UIParent);
+
+	local botY = parent:GetBottom()-(parent:GetHeight()*2);
+	local rightX = parent:GetRight()-100;
+	
+	
+	if cursorX > rightX then
+		local newWidth = parent:GetWidth()+(cursorX-rightX);
+		--parent:SetWidth(newWidth);
+	else
+		local newWidth = parent:GetWidth()-(rightX-cursorX);
+	--	parent:SetWidth(newWidth);
+	end
+	
+	--print(cursorY,"       ",botY);
+	
+end
 function printRage()
 	local rage = UnitPower("player");
-	testFont:SetText(getRage());
+	RagebarTextFont:SetText(getRage());
 end
-
 function getRage()
 	local rage = UnitPower("player");
 	return rage;
